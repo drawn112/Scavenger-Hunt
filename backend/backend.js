e74fb312b9011d023efc404e388c8a337a50886a
drawn112 <43252965+drawn112@users.noreply.github.com>
2019-06-24 14:57:27 -0400

    Add files via upload

---
+++ backend/backend.js
@@ -0,0 +1,17 @@
+function check(form)
+	{
+		// Andrew
+		if(form.userid.value == "Andrew" && form.pswrd.value == "backend-andr3w")
+		{
+			// Correct Password Location
+			location.replace('dashboard.html')
+    }
+    
+
+		// Josh
+		if(form.userid.value == "josh" && form.pswrd.value == "backend-j0sh")
+		{
+			// Correct Password Location
+            location.replace('dashboard.html')
+        }
+}
